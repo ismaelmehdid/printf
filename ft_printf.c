ec8228f06856fc2639357ffe784019ce7cdfc796
ismaelmehdid <ismaelmehdidwork@gmail.com>
2023-11-19 19:49:53 +0100

    printf good for one tester (now testing with others)

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ismaelmehdid <ismaelmehdid@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:42:33 by ismaelmehdi       #+#    #+#             */
-/*   Updated: 2023/11/19 00:28:57 by ismaelmehdi      ###   ########.fr       */
+/*   Updated: 2023/11/19 19:49:26 by ismaelmehdi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,29 @@
 
 int	ft_printformat(char thechar, va_list ap)
 {
-	int	count;
+	int		count;
+	void	*ptr;
 
 	count = 0;
 	if (thechar == 'c')
 		count += ft_retputchar(va_arg(ap, int));
-	if (thechar == 's')
+	else if (thechar == 's')
 		count += ft_retputstr(va_arg(ap, char *));
-	if (thechar == 'p')
+	else if (thechar == 'p')
 	{
+		ptr = va_arg(ap, void *);
+		if (ptr == NULL)
+			return (count += write (1, "(nil)", 5));
 		count += write (1, "0x", 2);
-		ft_retputnbr_hex((unsigned long)va_arg(ap, void *), &count, 'x');
+		ft_retputnbr_hex((unsigned long)ptr, &count, 'x');
 	}
-	if (thechar == 'd')
+	else if (thechar == 'd' || thechar == 'i')
 		ft_retputnbr(va_arg(ap, int), &count);
-	if (thechar == 'i')
-		ft_retputnbr(va_arg(ap, int), &count);
-	if (thechar == 'u')
+	else if (thechar == 'u')
 		ft_retputnbr_uint(va_arg(ap, unsigned int), &count);
-	if (thechar == 'x' || thechar == 'X')
+	else if (thechar == 'x' || thechar == 'X')
 		ft_retputnbr_hex((unsigned int)va_arg(ap, int), &count, thechar);
-	if (thechar == '%')
+	else if (thechar == '%')
 		count += write (1, "%", 1);
 	return (count);
 }
@@ -42,17 +44,25 @@ int	ft_printformat(char thechar, va_list ap)
 int	ft_printf(const char *format, ...)
 {
 	int		count;
+	int		i;
 	va_list	ap;
 
+	i = 0;
 	count = 0;
+	if (!format)
+		return (0);
 	va_start(ap, format);
-	while (*format)
+	while (format[i])
 	{
-		if (*format == '%')
-			count += ft_printformat(*(++format), ap);
+		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'
+				|| format[i + 1] == 'p' || format[i + 1] == 'd'
+				|| format[i + 1] == 'i'
+				|| format[i + 1] == 'u' || format[i + 1] == 'x'
+				|| format[i + 1] == 'X' || format[i + 1] == '%'))
+			count += ft_printformat(format[++i], ap);
 		else
-			count += write(1, format, 1);
-		format++;
+			count += write(1, &format[i], 1);
+		i++;
 	}
 	va_end(ap);
 	return (count);
--- ft_retputstr.c
+++ ft_retputstr.c
@@ -6,7 +6,7 @@
 /*   By: ismaelmehdid <ismaelmehdid@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 23:30:02 by ismaelmehdi       #+#    #+#             */
-/*   Updated: 2023/11/19 00:20:23 by ismaelmehdi      ###   ########.fr       */
+/*   Updated: 2023/11/19 16:01:41 by ismaelmehdi      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_retputstr(const char *str)
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (write(1, "(null)", 6));
 	while (str[i])
 		i += write(1, &str[i], 1);
 	return (i);
